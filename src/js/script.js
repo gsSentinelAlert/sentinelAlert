370b7b0b3e68a8722d9b65c4c646045461aaf8d3
Mariana <marianamalagutti@yahoo.com>
2026-05-29 00:17:24 -0300

    style: adicionando troca de tema

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// SLIDESHOW
 const slideImages = [
     './src/assets/foto1.png',
     './src/assets/foto2.png',
@@ -13,3 +14,23 @@ function trocarImagem(){
     slide.src = slideImages[imagemAtual];
 }
 setInterval(trocarImagem, 3000);
+
+const botoesTema = document.querySelectorAll(".theme-btn");
+
+botoesTema.forEach(function(botao){
+    botao.addEventListener("click", function(){
+        const tema = botao.dataset.theme;
+        if(tema == "blue"){
+            document.documentElement.style.setProperty("--bg-color", "#07111f");
+            document.documentElement.style.setProperty("--primary-color", "#2ea8ff");
+        }
+        if(tema == "green"){
+            document.documentElement.style.setProperty("--bg-color", "#07150f");
+            document.documentElement.style.setProperty("--primary-color", "#38d66b");
+        }
+        if(tema == "red"){
+            document.documentElement.style.setProperty("--bg-color", "#1a0a0a");
+            document.documentElement.style.setProperty("--primary-color", "#ff4d4d");
+        }
+    });
+});
